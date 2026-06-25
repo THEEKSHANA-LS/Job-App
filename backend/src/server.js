bf5c5bc3bbf47135dc43d2bb58d89d527c15fb62
THEEKSHANA L.S. <stheekshana90@gmail.com>
2026-06-25 23:48:23 +0530

    update port

--- backend/src/server.js
+++ backend/src/server.js
@@ -47,7 +47,7 @@ io.on("connection", (socket) => {
   });
 });
 
-const PORT = process.env.PORT || 5000;
+const PORT = process.env.PORT || 8080;
 
 httpServer.listen(PORT, () => {
   console.log(`Server running on port ${PORT}`);
